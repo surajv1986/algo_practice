01f0d20e5b2d603148b43987b476d75f0508e002
surajv1986 <surajv1986@gmail.com>
2020-05-23 16:40:39 +0530

    incomplete second half diagonal
    Signed-off-by: surajv1986 <surajv1986@gmail.com>

---
+++ half_rem_diag.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+#define N 4
+
+int r_ldiag(int, int, int);
+int l_rdiag(int, int, int);
+int main(void)
+{
+	int i = 0, j = 0;
+
+	printf("Enter index\n");
+	scanf("%d %d", &i, &j);
+	r_ldiag(i, j, N);
+	l_rdiag(i, j, N);
+}
+
+
+int r_ldiag(int i, int j, int n)
+{
+	printf("Entered index is i = %d, j = %d\n", i, j);
+	printf("Upper Matrix\n");
+	while (((i + j) >= 0) && (i > 0) && (j > 0)) {
+		i--;
+		j++;	
+		printf("i = %d j = %d\n", i, j);
+	}
+}
+
+
+int l_rdiag(int i, int j, int n)
+{
+	printf("Entered index is i = %d, j = %d\n", i, j);
+	printf("Lower Matrix\n");
+	while (((i + j) < (N + 2)) && (i < (N - 1)) && (j < (N - 1))) {
+		i++;
+		j--;	
+		printf("i = %d j = %d\n", i, j);
+	}
+}
