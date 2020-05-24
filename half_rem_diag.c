e422a55db2ea9a9a52844c30ca3e56ff06204e76
surajv1986 <surajv1986@gmail.com>
2020-05-24 14:12:19 +0530

    successfully implemented right-left diagonal element finding for a NXN matrix
    Signed-off-by: surajv1986 <surajv1986@gmail.com>

--- half_rem_diag.c
+++ half_rem_diag.c
@@ -19,9 +19,11 @@ int r_ldiag(int i, int j, int n)
 {
 	printf("Entered index is i = %d, j = %d\n", i, j);
 	printf("Upper Matrix\n");
-	while (((i + j) >= 0) && (i > 0) && (j > 0)) {
+	while (((i + j) > 0) && (i >= 0) && (j >= 0)) {
 		i--;
-		j++;	
+		j++;
+		if ((i < 0) || (j < 0) || (j > (N - 1) || (i > (N - 1)) ))	
+			break;
 		printf("i = %d j = %d\n", i, j);
 	}
 }
@@ -31,9 +33,11 @@ int l_rdiag(int i, int j, int n)
 {
 	printf("Entered index is i = %d, j = %d\n", i, j);
 	printf("Lower Matrix\n");
-	while (((i + j) < (N + 2)) && (i < (N - 1)) && (j < (N - 1))) {
+	while (((i + j) < (N + 2)) && (i < (N - 1)) && (j <= (N - 1)) && (i >= 0) && (j >= 0)) {
 		i++;
-		j--;	
+		j--;
+		if ((j < 0) || (i < 0))
+			break;	
 		printf("i = %d j = %d\n", i, j);
 	}
 }
